4eeeaef2fdc5490948726b6202f19dd5f0cbc812
souflero <dimitrios.soufleros@nokia.com>
2016-04-01 16:16:24 +0300

    - decoupled the url from the template

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8qvwf*c)ndngp%hoo$f7-=qjxz!esmfq8_mgl*(+8gqrgsm0t)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
