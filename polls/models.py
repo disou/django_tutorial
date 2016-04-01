1535cd1ff8948a58bb5a0bb04381779f1839b08a
souflero <dimitrios.soufleros@nokia.com>
2016-04-01 10:14:39 +0300

    Poll Application is added.

---
+++ polls/models.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+import datetime
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
+# Create your models here.
+
+# Each model is represented by a class that subclasses django.db.models.Model.
+# Each model has a number of class variables, each of which represents a database field in the mode
+
+# Each field is represented by an instance of a Field class - e.g.,
+# CharField for character fields and DateTimeField for datetimes.
+# This tells Django what type of data each field holds
+
+
+@python_2_unicode_compatible
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+@python_2_unicode_compatible
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
